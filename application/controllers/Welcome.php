0cf644538c5527ce6109219ef105b9c92a1ba30d
Terry <caixin0823@gmail.com>
2018-09-13 16:29:44 +0800

    test

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,7 @@ class Welcome extends CI_Controller
 	public function index($name='user1')
 	{
 		$data['name'] = $name;
-		echo 3; exit();
+		echo 4; exit();
 		$this->load->view('welcome_message',$data);
 	}
 }
